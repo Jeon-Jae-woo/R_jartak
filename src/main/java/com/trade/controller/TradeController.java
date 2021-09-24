00c3b0568da49a30b51a2f4ade7e450ecb4d8cce
just3377 <altmzla1@gmail.com>
2021-09-25 03:28:52 +0900

    [update] 거래

--- src/main/java/com/trade/controller/TradeController.java
+++ src/main/java/com/trade/controller/TradeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.trade.biz.TradeBiz;
+import com.trade.dto.TradeDto;
+import com.trade.dto.TradeUpdateDto;
 
 @Controller
 public class TradeController {
@@ -16,20 +18,34 @@ public class TradeController {
 	@Autowired
 	private TradeBiz tradebiz;
 	
-	
+	//1 번째 방법
 	@RequestMapping("/trade.do")
-	public String tread() {
-		logger.info("trade");
+	public String tread(Model model, @RequestParam("auction_no") int auction_no) {
+		logger.info("tradeselect one");
+		int selectNo = tradebiz.selectNo(auction_no);
+		model.addAttribute("TradeUpdateDto",tradebiz.selectNo(auction_no));
+		if(selectNo>0) {
+			return "mypage_sale_trading";
+		}else {
+			return "mypage_sele_end";
+		}
 		
-		return "trade";
+		
 	}
 	
-	@RequestMapping(value = "/sellerTradeList", method = RequestMethod.GET)
-	public String sellerTradeList(Model model,@RequestParam("nickname")String nickname) {
-		
-		return null;
-	}
-	
+//	//2번째 방법
+//	//1.auction_no값을 가지고 selectOne처리
+//	@RequestMapping("/trade.do")
+//	public String trade2(Model model,@RequestParam("auction_no") int auction_no) {
+//		TradeDto selectOne = tradebiz.selectOne(auction_no);
+//		model.addAttribute("selectOne", selectOne);
+//		int seller = TradeBiz.seller(selectOne.getSeller());
+//		int bidder = TradeBiz.bidder(selectOne.getBidder_nickname());
+//		int status = TradeBiz.status(selectOne.getAuction_no());
+//		return null;
+//		
+//	}
+//	
 	
 	
 	
--- src/main/webapp/WEB-INF/views/mypage_sale_trading.jsp
+++ src/main/webapp/WEB-INF/views/mypage_sale_trading.jsp
@@ -94,7 +94,7 @@
                                   <td>${list.endDateStr}</td>
                                   <td>${auctionlist[status.index].bidder_nickname}</td>
                                   <td><button></button></td>
-                                  <td><button></button></td>
+                                  <td><button onclick="location.href='trade.do?${list.auction.no}'"></button></td>
 
                                 </tr>
                               
